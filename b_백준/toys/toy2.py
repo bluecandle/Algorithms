ea089ec0072194a269362a94076796515bc47fe7
jinyeongkim <jinyeongkim37@gmail.com>
2020-09-23 18:32:01 +0900

    boj init

---
+++ b_백준/toys/toy2.py
@@ -0,0 +1,40 @@
+# nums = input().split(' ')
+# num_1 = int(nums[0])
+# num_2 = int(nums[1])
+
+# print(num_1+num_2)
+
+
+# nums = input().split(' ')
+# num_1 = int(nums[0])
+# num_2 = int(nums[1])
+
+# print(num_1 + num_2)
+# print(num_1 - num_2)
+# print(num_1 * num_2)
+# print(num_1 // num_2)
+# print(num_1 % num_2)
+
+
+# nums = input().split(' ')
+# A = int(nums[0])
+# B = int(nums[1])
+# C = int(nums[2])
+
+# results =[
+#      (A+B)%C,
+#      ((A%C) + (B%C))%C,
+#      (A*B)%C,
+#      ((A%C) * (B%C))%C
+# ]
+
+# for r in results:
+#     print(r)
+
+# num_1 = int(input())
+# num_2 = input()
+
+# for i in range(2,-1,-1):
+#     print(num_1 * int(num_2[i]))
+
+# print(num_1*int(num_2))
